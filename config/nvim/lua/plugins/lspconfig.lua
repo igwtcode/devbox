577fdf13f46c7d8590cb31c6c530525fd91c9a7c
igwtcode <93984311+igwtcode@users.noreply.github.com>
2025-01-24 22:05:33 +0100

    not imp

--- config/nvim/lua/plugins/lspconfig.lua
+++ config/nvim/lua/plugins/lspconfig.lua
@@ -383,8 +383,8 @@ return {
         'stylua', -- Used to format Lua code
       })
 
-      vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, { border = 'rounded' })
-      vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = 'rounded' })
+      vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, { border = 'single' })
+      vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = 'single' })
 
       local icons = {
         Error = ' ',
