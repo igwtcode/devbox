46b54e4f0b30b93479d7e416fce455f03dc4f0b1
igwtcode <93984311+igwtcode@users.noreply.github.com>
2025-02-17 20:55:13 +0100

    nvim+arch: no bg blur

--- config/nvim/lua/plugins/catppuccin.lua
+++ config/nvim/lua/plugins/catppuccin.lua
@@ -11,11 +11,11 @@ return {
           light = 'latte',
           dark = 'mocha',
         },
-        transparent_background = true, -- disables setting the background color.
+        transparent_background = false, -- disables setting the background color.
         show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
         term_colors = true, -- sets terminal colors (e.g. `g:terminal_color_0`)
         dim_inactive = {
-          enabled = false, -- dims the background color of inactive window
+          enabled = true, -- dims the background color of inactive window
           shade = 'dark',
           percentage = 0.39, -- percentage of the shade to apply to the inactive window
         },
