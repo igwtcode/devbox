378586f4c3f71cc14f89ba043c21dc3960cc0166
igwtcode <93984311+igwtcode@users.noreply.github.com>
2025-03-03 15:34:58 +0100

    nvim: root_spec (dir)

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -13,5 +13,5 @@ opt.relativenumber = false -- Relative line numbers
 -- * the name of a detector function like `lsp` or `cwd`
 -- * a pattern or array of patterns like `.git` or `lua`.
 -- * a function with signature `function(buf) -> string|string[]`
--- vim.g.root_spec = { "lsp", { ".git", "lua" }, "cwd" }
-vim.g.root_spec = { ".git", "cwd" }
+vim.g.root_spec = { "lsp", { ".git", "lua" }, "cwd" }
+-- vim.g.root_spec = { ".git", "cwd" }
