487bc4704e39d4d991dd4d4be2cf61dcd65a86a6
igwtcode <93984311+igwtcode@users.noreply.github.com>
2025-03-06 17:27:22 +0100

    nvim cmd,lsp
    cfn,sam templates
    tf files

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -2,5 +2,10 @@ return {
   "neovim/nvim-lspconfig",
   opts = {
     inlay_hints = { enabled = false },
+    servers = {
+      yamlls = {
+        filetypes = { "yaml", "yaml.ansible", "yaml.github", "yaml.cfn", "yaml.sam" },
+      },
+    },
   },
 }
