11fba93e294572b68befaa860e24a734c20bf248
igwtcode <93984311+igwtcode@users.noreply.github.com>
2025-10-26 21:40:00 +0100

    nvim: conform
    format

--- config/nvim/lua/plugins/conform.lua
+++ config/nvim/lua/plugins/conform.lua
@@ -4,12 +4,18 @@ return {
     ---@type conform.setupOpts
     local opts = {
       formatters_by_ft = {
+        terraform = { 'terraform_fmt' },
+        tf = { 'terraform_fmt' },
+        ['terraform-vars'] = { 'terraform_fmt' },
         ["javascript"] = { "biome", "prettier" , stop_after_first = true},
         ["javascriptreact"] = { "biome", "prettier" , stop_after_first = true},
         ["typescript"] = { "biome", "prettier" , stop_after_first = true},
         ["typescriptreact"] = { "biome", "prettier" , stop_after_first = true},
         ["json"] = { "biome", "prettier" , stop_after_first = true},
+        ["jsonc"] = { "biome", "prettier" , stop_after_first = true},
         ["css"] = { "biome", "prettier" , stop_after_first = true},
+        ["markdown"] = { "prettier" , stop_after_first = true},
+        ["md"] = { "prettier" , stop_after_first = true},
       },
       -- The options you set here will be merged with the builtin formatters.
       -- You can also define any custom formatters here.
