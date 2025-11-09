f3d1fe75f81eb497cbd949d745b7d2ff2a2e6451
igwtcode <93984311+igwtcode@users.noreply.github.com>
2025-11-09 13:05:23 +0100

    nvim: edit conform

--- config/nvim/lua/plugins/conform.lua
+++ config/nvim/lua/plugins/conform.lua
@@ -3,19 +3,30 @@ return {
   opts = function()
     ---@type conform.setupOpts
     local opts = {
+      default_format_opts = {
+        timeout_ms = 3000,
+        async = false, -- not recommended to change
+        quiet = false, -- not recommended to change
+        lsp_format = "fallback", -- not recommended to change
+      },
       formatters_by_ft = {
-        terraform = { 'terraform_fmt' },
-        tf = { 'terraform_fmt' },
-        ['terraform-vars'] = { 'terraform_fmt' },
-        ["javascript"] = { "biome", "prettier" , stop_after_first = true},
-        ["javascriptreact"] = { "biome", "prettier" , stop_after_first = true},
-        ["typescript"] = { "biome", "prettier" , stop_after_first = true},
-        ["typescriptreact"] = { "biome", "prettier" , stop_after_first = true},
-        ["json"] = { "biome", "prettier" , stop_after_first = true},
-        ["jsonc"] = { "biome", "prettier" , stop_after_first = true},
-        ["css"] = { "biome", "prettier" , stop_after_first = true},
-        ["markdown"] = { "prettier" , stop_after_first = true},
-        ["md"] = { "prettier" , stop_after_first = true},
+        ["css"] = { "biome", "prettier", stop_after_first = true },
+        fish = { "fish_indent" },
+        ["javascript"] = { "biome", "prettier", stop_after_first = true },
+        ["javascriptreact"] = { "biome", "prettier", stop_after_first = true },
+        ["json"] = { "biome", "prettier", stop_after_first = true },
+        ["jsonc"] = { "biome", "prettier", stop_after_first = true },
+        lua = { "stylua" },
+        ["markdown"] = { "prettier", stop_after_first = true },
+        ["md"] = { "prettier", stop_after_first = true },
+        sh = { "shfmt" },
+        terraform = { "terraform_fmt" },
+        ["terraform-vars"] = { "terraform_fmt" },
+        tf = { "terraform_fmt" },
+        ["typescript"] = { "biome", "prettier", stop_after_first = true },
+        ["typescriptreact"] = { "biome", "prettier", stop_after_first = true },
+        yaml = { "prettier" },
+        yml = { "prettier" },
       },
       -- The options you set here will be merged with the builtin formatters.
       -- You can also define any custom formatters here.
