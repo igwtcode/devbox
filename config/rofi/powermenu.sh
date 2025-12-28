b5244f83053ff6b6d249145adf9437cd60478a72
igwtcode <93984311+igwtcode@users.noreply.github.com>
2025-12-28 22:02:56 +0100

    fix rofi

--- config/rofi/powermenu.sh
+++ config/rofi/powermenu.sh
@@ -2,32 +2,29 @@
 # vim: ft=bash
 
 conf=~/.config/rofi/powermenu.rasi
-options="  Lock\n󰤄  Suspend\n󰒲  Hibernate\n󰜉  Reboot\n  Shutdown\n󰗽  Logout"
+options="  Lock\n󰤄  Suspend\n󰜉  Reboot\n  Shutdown\n󰗽  Logout"
 
 selected_option=$(echo -e "$options" | rofi -dmenu -i -config $conf)
 selected_option=$(echo $selected_option | cut -d " " -f 2)
 
 case $selected_option in
-  Lock)
-    hyprlock
-    ;;
-  Suspend)
-    pkexec systemctl suspend
-    ;;
-  Hibernate)
-    pkexec systemctl hibernate
-    ;;
-  Reboot)
-    pkexec systemctl reboot
-    ;;
-  Shutdown)
-    pkexec systemctl poweroff
-    ;;
-  Logout)
-    # TODO: Check this
-    hyprctl dispatch exit
-    ;;
-  *)
-    exit 1
-    ;;
+Lock)
+  hyprlock
+  ;;
+Suspend)
+  pkexec systemctl suspend
+  ;;
+Reboot)
+  pkexec systemctl reboot
+  ;;
+Shutdown)
+  pkexec systemctl poweroff
+  ;;
+Logout)
+  # TODO: Check this
+  hyprctl dispatch exit
+  ;;
+*)
+  exit 1
+  ;;
 esac
