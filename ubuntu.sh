27cc6490aaa0337d26ca1e83f11f01537fabbfb2
igwtcode <93984311+igwtcode@users.noreply.github.com>
2025-12-30 09:21:02 +0100

    fix

--- ubuntu.sh
+++ ubuntu.sh
@@ -14,7 +14,7 @@ update_apt() {
 }
 
 add_docker_apt_repo() {
-  sudo apt install ca-certificates curl
+  sudo apt install -y ca-certificates curl
   sudo install -m 0755 -d /etc/apt/keyrings
   sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
   sudo chmod a+r /etc/apt/keyrings/docker.asc
